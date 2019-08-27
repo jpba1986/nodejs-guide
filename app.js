b9817328d9abfc16bedfdd6827003c2750bcb2de
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-08-27 19:00:41 -0400

    sending responses (basic)

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=> {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head></head>');
+    res.write('<body>');
+    res.write('<p>https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers</p>');
+    res.write('<body>');
+    res.write('</html>');
+    res.end();
+
 });
 
 
