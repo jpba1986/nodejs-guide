89b28530c171c322c0d9acefb9ad7a6f0bce34e2
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-08-27 19:06:51 -0400

    routing requests

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const http = require('http');
 const server = http.createServer((req, res)=> {
     console.log(req.url, req.method, req.headers);
 
+    const url = req.url;
+
+    if (url ==='/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head></head>');
+        res.write('<body>');
+        res.write('<p>Im root</p>');
+        res.write('<body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head></head>');
