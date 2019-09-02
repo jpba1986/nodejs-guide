82f3d6d78174027d2d424a8149e9feb6857545d1
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-02 15:56:13 -0400

    app route

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const http = require('http');
-
 const express = require('express');
 
 const app =  express();
 
-app.use((req, res, next) => {
-    console.log('im here !');
-    next(); // allows the request to continue to the next middleware in line
+app.use('/',(req, res, next) => {
+    console.log('im always here !!');    
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
     console.log('im here again !!!!');    
+    res.send('<h1>im add product</h1>');
+});
+
+app.use('/',(req, res, next) => {
+    console.log('im here again !!!!');    
+    res.send('<h1>Hi from express</h1>');
 });
 
 
-const server = http.createServer(app);
-
-server.listen(3001);
+app.listen(3001);
