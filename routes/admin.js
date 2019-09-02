8136099d607519e83842417930452c25f62e6f1b
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-02 17:48:01 -0400

    creating views

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product',(req, res, next) => {  
-    res.send('<form action="/admin/product" method="POST"><input type="text" name ="title"> <button type="submit">Add product </button> </form>');
+    res.send(
+        '<form action="/admin/product" method="POST"><input type="text" name ="title"> <button type="submit">Add product </button> </form>'
+        );
 });
 
 router.post('/product',(req,res, next)=>{
