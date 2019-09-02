17d60198cfd095cd279a48279c1eec384d47c884
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-02 18:22:35 -0400

    serving html pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.send('<h1>Hi from shop route</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views','shop.html'));
 });
 
 
