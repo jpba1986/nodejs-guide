f0a9e0bc097d5ea5081e43025b5075ce903591b5
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-04 16:23:44 -0400

    adding controllers

---
+++ controllers/products.js
@@ -0,0 +1,28 @@
+const product = [];
+
+exports.getAddProduct = (req, res, next) => {  
+    res.render('add-product', { 
+        pageTitle: 'Add Product', 
+        path:'/admin/add-product', 
+        formsCSS: true, 
+        productCSS: true, 
+        activeAddProduct: true 
+    });
+};
+
+
+exports.postAddProducts = (req,res, next)=>{
+    product.push({ title: req.body.title });
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+    res.render('shop', { 
+        prods: product, 
+        pageTitle: 'Shop', 
+        path:'/', 
+        hasProducts: product.length > 0,
+        activeShop: true,
+        productCSS: true      
+    });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,11 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const product = [];
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product',(req, res, next) => {  
-    res.render('add-product', { 
-        pageTitle: 'Add Product', 
-        path:'/admin/add-product', 
-        formsCSS: true, 
-        productCSS: true, 
-        activeAddProduct: true 
-    });
-});
+router.post('/add-product', productsController.postAddProducts);
 
-router.post('/add-product',(req,res, next)=>{
-    console.log(req.body);
-    product.push({ title: req.body.title });
-    res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = product;
+module.exports = router;
