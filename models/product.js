e29541fba5884af3cf74d11c7eee56401358feb7
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-04 17:09:30 -0400

    fetching data from a file via the model

--- models/product.js
+++ models/product.js
@@ -1,15 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
 const products = [];
+
 module.exports = class Product {
     constructor(t){
         this.title = t;
     }
 
     save(){
-        products.push(this);
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data',
+            'products.json');
+        fs.readFile(p, (err, fileContent) => {            
+            let products = [];
+            if(!err){
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err)=>{
+                console.log(err);
+            });
+        });
+        //products.push(this);
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data',
+            'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            if (err){
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });        
     }
-
-}
+};
