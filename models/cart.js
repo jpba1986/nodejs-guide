9661c04830aaa80bff74eb22b085ef7dd734393e
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-12 18:25:31 -0300

    deleting cart items and displaying item  on cart

--- models/cart.js
+++ models/cart.js
@@ -33,7 +33,37 @@ module.exports = class Cart {
             cart.totalPrice = cart.totalPrice + +productPrice;
             fs.writeFile(p, JSON.stringify(cart), (err)=>{
                 console.log(err);
-            })
+            });
         });
     }
-}
+
+
+    static deleteProduct(id, ProductPrice){
+        fs.readFile(p, (err, fileContent)=>{
+            if(err){
+                return;
+            }
+            const updatedCart = { ...JSON.parse(fileContent)};
+            const product = updatedCart.products.findeIndex(prod => prod.id === id);
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id );
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+            fs.writeFile(p, JSON.stringify(cart), (err)=>{
+                console.log(err);
+            });
+
+        });
+    }
+
+    static getCart(cb){
+        fs.readFile(p, (err, fileContent)=>{
+            const cart = JSON.parse(fileContent);
+            if (err){
+                cb(null);
+            }else{
+                cb(cart);
+            }            
+        });
+    }
+};
--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
     path.dirname(process.mainModule.filename), 
     'data',
@@ -35,6 +37,7 @@ module.exports = class Product {
                     console.log(err);
                 });
             }else{
+                this.id = Math.random().toString();
                 products.push(this);
                 fs.writeFile(p, JSON.stringify(products), (err)=>{
                     console.log(err);
@@ -44,15 +47,15 @@ module.exports = class Product {
     }
 
 
-    static deleteById(){
+    static deleteById(id){
         getProductsFromFile(products =>{
+            const product = products.find(prod => prod.id === id);
             const updatedProducts = products.filter(prod => prod.id !== id );
             fs.writeFile(p, JSON.stringify(updatedProducts), err =>{
                 if(!err){
-                    
+                    Cart.deleteProduct(id, product.price);
                 }
             });
-            cb(product);
         });
     }
 
