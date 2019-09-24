82e74790efc3359fdb7e4fb472bb38c7af33628d
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-24 11:27:05 -0300

    add order items

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -109,12 +108,32 @@ exports.postCartDeleteProduct = (req, res, next) => {
     res.redirect('/cart');  
   })
   .catch(err => console.log(err));
-/* 
-  Product.findByPk(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect('/cart');
-  }); */
 };
+
+exports.postOrder = (req, res, next) =>{
+  req.user
+  .getCart()
+  .then(cart =>{
+    return cart.getProducts();
+  })
+  .then(products =>{
+    return req.user
+    .createOrder()
+    .then( order =>{
+      order.addProduct(product.map( product =>{
+        product.orderItem = { quantity: product.cartItem.quantity};
+        return product;
+      }));
+    })
+    .catch(err => console.log(err));
+    //console.log(products);
+  })
+  .then(result =>{
+    res.redirect('/orders');
+  })
+  .catch(err => console.log(err));
+};
+
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
