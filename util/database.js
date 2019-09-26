a84f86e72278f1c55796d01abd7177a2650ec31f
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-26 18:25:37 -0300

    mongo connect part1

--- util/database.js
+++ util/database.js
@@ -1,7 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete','root','nodecomplete1',{
-    dialect: 'mysql', host: 'localhost' 
-});
+const mongoConnect = (cb) =>{
+    MongoClient.connect('mongodb+srv://jp:mongoJP1986@cluster0-c95ia.mongodb.net/test?retryWrites=true&w=majority')
+    .then( client => {
+        console.log('Connected');
+        cb(client);
+    })
+    .catch(err => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
