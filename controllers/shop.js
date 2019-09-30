f17f41c49755ad213f5a62ecf0d28a3f920f3438
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-09-30 15:32:44 -0300

    displaying the cart items. Part1

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,28 +53,24 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
+    .then(products => {
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
             products: products
           });
         })
-        .catch(err => console.log(err));
-    })
     .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId).then( product =>{
-    return req.user.addToCart(product);
+    return req.user.addToCart(product);    
   })
   .then(result =>{
     console.log(result);
+    res.redirect('/cart');
   });
   /* let fetchedCart;
   let newQuantity = 1;
--- routes/shop.js
+++ routes/shop.js
@@ -10,8 +10,8 @@ router.get('/products', shopController.getProducts);
  
 router.get('/products/:productId', shopController.getProduct);
 
-/* router.get('/cart', shopController.getCart);
- */
+router.get('/cart', shopController.getCart);
+
 router.post('/cart', shopController.postCart);
 /*
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
