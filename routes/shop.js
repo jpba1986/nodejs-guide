e4fe7855fb03464a001abaf067a17584b109ca5e
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-04 08:17:56 -0300

    fetch all products

--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,10 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-/* router.get('/', shopController.getIndex);
+ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
- 
+ /*
 router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
