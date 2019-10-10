eb10ef6ccf254162497fc272c5c1edff3f1895d1
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-10 15:25:42 -0300

    deleting a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,3 +18,9 @@ exports.postLogin = (req, res, next) => {
         .catch(err => console.log(err));
 
 };
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(()=>{
+    res.redirect('/'); 
+  });
+};
