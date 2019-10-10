2c648c70d0e0c17bb43bfac0ec8857264d36a4aa
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-10 11:23:17 -0300

    store sessions mongoDB

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://jp:StHp4Uf2RjvD27DD@cluster0-c95ia.mongodb.net/shop';
 const app = express();
+
+const store = new MongoDBStore({
+    uri:MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -22,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret:'my secret',
     resave: false,
-    saveUninitialized: false    
+    saveUninitialized: false,
+    store: store    
 }));
 app.use((req, res, next) => {
   User.findById('5d974ae6c85e472b80598b12')
@@ -40,7 +48,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-.connect('mongodb+srv://jp:StHp4Uf2RjvD27DD@cluster0-c95ia.mongodb.net/shop?retryWrites=true&w=majority')
+.connect(MONGODB_URI)
 .then(result =>{
 
     User.findOne().then(user =>{
