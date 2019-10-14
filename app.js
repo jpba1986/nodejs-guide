82a624a16c8adc2ec78c3ce3624a64b351742208
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-14 16:35:27 -0300

    providing user feedback

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash =require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -20,6 +21,7 @@ const store = new MongoDBStore({
 });
 
 const csrfProtection = csrf();
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -40,6 +42,7 @@ app.use(
 );
 
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
