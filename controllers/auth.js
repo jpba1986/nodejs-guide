18f39118b24bbac0306d9bb62058ef291c6c3ca7
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-14 17:29:13 -0300

    reset password template

--- controllers/auth.js
+++ controllers/auth.js
@@ -120,3 +120,19 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req,res,next)=>{
+  let  message = req.flash('error');
+  console.log(message);
+ if (message.length > 0 ){
+   message = message[0];
+ }
+ else{
+   message = null;
+ } 
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage : message
+  });
+};
