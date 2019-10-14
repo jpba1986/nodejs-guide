6f1527c2bc331052885bea74c51b6b76825cb3cf
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-14 17:22:32 -0300

    catch error for send emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,37 +77,41 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-
-  User.findOne({ email: email})
-  .then( userDoc => {
-    if (userDoc){      
-      req.flash('error','Email exists already.');
-      return res.redirect('/signup');
-    }
-    return bcrypt
-    .hash(password,12)
-    .then(hashedPassword=>{
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items :[] }    
-      });
-      return user.save();
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash(
+          'error',
+          'E-Mail exists already, please pick a different one.'
+        );
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
     })
-    .then(result =>{      
-      res.redirect('/login');
-      transporter.sendMail({
-        to: email,
-        from : 'shop@node-complete.com',
-        subject: 'Signup succeeded',
-        html: '<h1>You successfully signed up!</h1>'
-      });
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err =>{ 
-    console.log(err) 
-  } );
-  
 };
 
 exports.postLogout = (req, res, next) => {
