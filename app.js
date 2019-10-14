ec49c43c4acefba27118c1b96904fec1f87506fa
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-14 09:03:41 -0300

    starting setup

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://jp:StHp4Uf2RjvD27DD@cluster0-c95ia.mongodb.net/shop';
-const app = express();
+const MONGODB_URI =
+  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop';
 
+const app = express();
 const store = new MongoDBStore({
-    uri:MONGODB_URI,
-    collection: 'sessions'
+  uri: MONGODB_URI,
+  collection: 'sessions'
 });
 
 app.set('view engine', 'ejs');
@@ -26,34 +27,26 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-    secret:'my secret',
+app.use(
+  session({
+    secret: 'my secret',
     resave: false,
     saveUninitialized: false,
-    store: store    
-}));
+    store: store
+  })
+);
 
-app.use((req,res,next)=>{
-    if (!req.session.user){
-        return next();
-    }
-    User.findById(req.session.user._id)
-    .then(user => {
-        req.user = user;
-      next();       
-    })
-    .catch(err => console.log(err));
-});
-
-
-/* app.use((req, res, next) => {
-  User.findById('5d974ae6c85e472b80598b12')
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
     .catch(err => console.log(err));
-}); */
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -62,21 +55,22 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-.connect(MONGODB_URI)
-.then(result =>{
-
-    User.findOne().then(user =>{
-        if (!user){
-            const user = new User({
-                name: 'JP',
-                email: 'jp@test.com',
-                cart: {
-                    items:[]
-                }
-            });
-            user.save();
-        }
+  .connect(MONGODB_URI)
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
     });
-
-    app.listen(3002);
-}).catch(err => console.log(err));
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/admin.js
+++ controllers/admin.js
@@ -25,7 +25,7 @@ exports.postAddProduct = (req, res, next) => {
     .save()
     .then(result => {
       // console.log(result);
-      //console.log('Created Product');
+      console.log('Created Product');
       res.redirect('/admin/products');
     })
     .catch(err => {
@@ -71,7 +71,7 @@ exports.postEditProduct = (req, res, next) => {
       return product.save();
     })
     .then(result => {
-      //console.log('UPDATED PRODUCT!');
+      console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
@@ -82,7 +82,7 @@ exports.getProducts = (req, res, next) => {
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then(products => {
-      //console.log(products);
+      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
@@ -97,7 +97,7 @@ exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findByIdAndRemove(prodId)
     .then(() => {
-      //console.log('DESTROYED PRODUCT');
+      console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
