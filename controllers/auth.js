6e95ac8f603526de028ef8bdc631645aee93ef9d
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-14 17:19:57 -0300

    using nodemailer to send an email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key:'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'
+  }
+}));
 
 exports.getLogin = (req, res, next) => {
    let  message = req.flash('error');
@@ -86,8 +94,14 @@ exports.postSignup = (req, res, next) => {
       });
       return user.save();
     })
-    .then(result =>{
+    .then(result =>{      
       res.redirect('/login');
+      transporter.sendMail({
+        to: email,
+        from : 'shop@node-complete.com',
+        subject: 'Signup succeeded',
+        html: '<h1>You successfully signed up!</h1>'
+      });
     });
   })
   .catch(err =>{ 
