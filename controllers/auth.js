41089af3abe7422fdfc0f64a020252706df1c3b9
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-15 17:56:02 -0300

    login validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,7 +24,12 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage : message
+    errorMessage : message,
+    oldInput: {
+      email:'',
+      password:''
+    },
+    validationErrors: []
   });
 };
 
@@ -59,7 +64,12 @@ exports.postLogin = (req, res, next) => {
     return res.status(422).render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
-      errorMessage : errors.array()[0].msg
+      errorMessage : errors.array()[0].msg,
+      oldInput: {
+        email:email,
+        password: password
+      },
+      validationErrors: errors.array()      
     });
   }
 
@@ -67,8 +77,16 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email})
     .then(user => {
       if (!user){
-        req.flash('error','Invalid email or password.');
-        return res.redirect('/login'); 
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage : errors.array()[0].msg,
+          oldInput: {
+            email:email,
+            password: password
+          },
+          validationErrors: []      
+        });
       }
       bcrypt
       .compare(password, user.password)
@@ -81,8 +99,16 @@ exports.postLogin = (req, res, next) => {
             res.redirect('/');
           });
         }
-        req.flash('error','Invalid email or password.');
-        res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage : errors.array()[0].msg,
+          oldInput: {
+            email:email,
+            password: password
+          },
+          validationErrors: []      
+        });
       })
       .catch(err => {
         console.log(err);
