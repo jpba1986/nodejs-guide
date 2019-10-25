bf8de48b24667d8e809d93b7e976242f9c7f5665
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-10-25 11:12:35 -0300

    files

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,9 +14,25 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.file;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+ console.log(req.file);
+  if (!image){
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price:price,
+        description:description
+      },
+      errorMessage: 'Attached file is not an image',
+      validationErrors: []
+    });
+  }
   const errors = validationResult(req);
 
   if(!errors.isEmpty()){
@@ -27,7 +43,6 @@ exports.postAddProduct = (req, res, next) => {
       hasError: true,
       product: {
         title: title,
-        imageUrl: imageUrl,
         price:price,
         description:description
       },
@@ -35,6 +50,10 @@ exports.postAddProduct = (req, res, next) => {
       validationErrors: errors.array()
     });
   }
+
+  const imageUrl = image.path;
+
+
 
   const product = new Product({
     title: title,
