ccb563b3b2f4b7a58f176bbab74f1808c0188d5b
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-11-05 11:23:58 -0300

    using PDFKIT for pdf generation

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
+const PDFDocument = require('pdfkit');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -154,13 +155,18 @@ exports.getInvoice = (req,res,next) =>{
     }
     const invoiceName = 'invoice-' + orderId+ 'pdf';
     const invoicePath = path.join('data', 'invoices', invoiceName);
-      const file = fs.createReadStream(invoicePath);
-      res.setHeader('Content-Type','application/pdf');
+
+    const pdfDoc = new PDFDocument();
+    res.setHeader('Content-Type','application/pdf');
       res.setHeader(
         'Content-Disposition',
         'inline: filename="' + invoiceName + '"'
       );
-      file.pipe(res);
+    pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+
+    pdfDoc.text('Hi there');
+    pdfDoc.end();
   })
   .catch(err => next(err));
 };
