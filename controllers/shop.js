b4aa8314796fc336445bf3ed0ac2bd2af44e0997
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-11-07 10:44:43 -0300

    adding dynamic pagination btns

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
 const PDFDocument = require('pdfkit');
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -39,7 +39,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1 ;
   let totalItems;
   Product.find()
   .countDocuments()
@@ -54,7 +54,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        totalProducts : totalItems,
+        currentPage : page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1, 
