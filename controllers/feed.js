7d1110c4ed40ac53940f7cf3a951511bf0304e0e
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-11-12 12:55:22 -0300

    delete post

--- controllers/feed.js
+++ controllers/feed.js
@@ -132,6 +132,29 @@ exports.updatePost = (req, res, next) => {
 
 };
 
+exports.deletePost =(req,res,next)=>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post =>{     
+        if (!post){
+            const error = new Error('No post');
+            error.statusCode = 404;
+            throw error;
+        }
+        // check logged user
+        clearImage(post.imageUrl);
+        return Post.findByIdAndRemove(postId);
+    })
+    .then(result =>{
+        res.status(200).json({ message:'Deleted ok'});
+    })
+    .catch(err =>{
+        if (!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
 const clearImage = filePath =>{
     filePath = path.join(__dirname, '..', filePath);
     fs.unlink(filePath, err => console.log(err));
