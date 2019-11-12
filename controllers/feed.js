25da4c2c47fa03f1f39ef87ee459580c57796e1f
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-11-12 09:12:43 -0300

    adding server side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+
+const { validationResult  } = require('express-validator');
+
 exports.getPosts = (req,res,next) =>{
     res.status(200).json({
         posts:[{ 
@@ -14,6 +17,16 @@ exports.getPosts = (req,res,next) =>{
 }
 
 exports.createPost = (req,res,next) =>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res
+            .status(422)
+            .json({ 
+                message: 'Validation failed', 
+                errors: errors.array()
+            }
+        );
+    }
     const title = req.body.title;
     const content = req.body.content;
     res.status(201).json({ // 201 success creating resource
