4d9d5c0a2d72deeea32966a6fe607617c9a18a6a
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-11-13 10:30:40 -0300

    adding pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,17 +5,31 @@ const { validationResult  } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req,res,next) =>{
-    Post.find()
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
+    Post.find().countDocuments()
+    .then(count =>{
+        totalItems = count;
+        return Post.find()
+        .skip((currentPage -1 ) * perPage)
+        .limit(perPage);
+    })        
     .then(posts =>{
-        res.status(200).json({ message: 'Fetched!', posts: posts});
+        res
+        .status(200)
+        .json({ 
+            message: 'Fetched!', 
+            posts: posts,
+            totalItems: totalItems
+        });
     })
     .catch(err =>{
         if (!err.statusCode){
             err.statusCode = 500;
         }
         next(err);
-    });
-
+    });  
 };
 
 exports.createPost = (req,res,next) =>{
