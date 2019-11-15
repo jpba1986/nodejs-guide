42ad10957cb3e2dc74fde6de662b81977a422e8e
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-11-15 11:33:11 -0300

    defining a mutation schema

--- app.js
+++ app.js
@@ -57,7 +57,8 @@ app.use(
     '/graphql', 
     graphqlHttp({
       schema : graphqlSchema,
-      rootValue : graphqlResolver
+      rootValue : graphqlResolver,
+      graphiql: true
     }
   )
 );
