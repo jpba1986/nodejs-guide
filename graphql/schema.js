990a5839e0abf4125113f86ab3e861daaf152ab4
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-11-18 14:58:39 -0300

    get PostData

--- graphql/schema.js
+++ graphql/schema.js
@@ -25,6 +25,10 @@ module.exports = buildSchema(`
         userId: String!
     }
 
+    type PostData{
+        posts: [Post!]!
+        totalPosts: Int!
+    }
     input UserInputData{
         email: String!
         name: String!
@@ -39,6 +43,7 @@ module.exports = buildSchema(`
 
     type RootQuery{
         login(email: String! , password:String!): AuthData!
+        posts: PostData!
     }
 
     type RootMutation{
