37990c452e2bec0b71278343fd08b25069266462
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-11-19 15:57:23 -0300

    npm install --save compression

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const flash =require('connect-flash');
 const multer = require('multer');
 
 const helmet = require('helmet');
+const compression = require('compression');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -54,6 +55,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(helmet());
+app.use(compression());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
